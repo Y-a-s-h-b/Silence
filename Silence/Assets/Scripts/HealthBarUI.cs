108b87da1c5087b666d59b32db0582d3289bc759
Yash <89540665+Y-a-s-h-b@users.noreply.github.com>
2025-01-07 23:17:04 +0530

    UI

---
+++ Silence/Assets/Scripts/HealthBarUI.cs
@@ -0,0 +1,22 @@
+using MoreMountains.Tools;
+using RengeGames.HealthBars;
+using UnityEngine;
+
+public class HealthBarUI : MonoBehaviour
+{
+    public RadialSegmentedHealthBar healthBar;
+    public MMProgressBar progressBar;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        healthBar = GetComponent<RadialSegmentedHealthBar>();
+        progressBar = GetComponentInParent<MMProgressBar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(progressBar.BarProgress);
+        healthBar.RemoveSegments.Value = healthBar.SegmentCount.Value - (progressBar.BarProgress * healthBar.SegmentCount.Value);
+    }
+}
