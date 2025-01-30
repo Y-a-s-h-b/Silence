93307e5c63589b0a7807d22be327d3f97f79f22c
AnkitGurjar6633 <ankitgurjar6633@gmail.com>
2025-01-30 20:41:03 +0530

    removed AbilityManager

--- Silence/Assets/Scripts/AbilityTrigger.cs
+++ Silence/Assets/Scripts/AbilityTrigger.cs
@@ -1,6 +1,8 @@
 using MoreMountains.CorgiEngine;
 using UnityEngine;
 
+public enum Abilities { Jump, Sprint, Dash }
+
 public class AbilityTrigger : MonoBehaviour
 {
     public Abilities[] AbilitiesToToggle;
@@ -16,7 +18,7 @@ public class AbilityTrigger : MonoBehaviour
         {
             foreach (var ability in AbilitiesToToggle)
             {
-                AbilityManager.Instance.DisableAbility(ability);
+                DisableAbility(ability, other.gameObject);
             }
 
             if (SlowPlayer)
@@ -34,7 +36,7 @@ public class AbilityTrigger : MonoBehaviour
         {
             foreach (var ability in AbilitiesToToggle)
             {
-                AbilityManager.Instance.EnableAbility(ability);
+                EnableAbility(ability, other.gameObject);
             }
 
             if (SlowPlayer)
@@ -44,5 +46,62 @@ public class AbilityTrigger : MonoBehaviour
                 //movementController.AbilityMovementSpeedMultiplier = 1f;
             }
         }
+    }
+
+
+    private void EnableAbility(Abilities ability, GameObject player)
+    {
+        if (ability.Equals(Abilities.Sprint))
+        {
+            SetSprintAbility(player, true);
+        }
+
+        else if (ability.Equals(Abilities.Jump))
+        {
+            SetJumpAbility(player, true);
+        }
+
+        else if (ability.Equals(Abilities.Dash))
+        {
+            SetDashAbility(player, true);
+        }
+    }
+
+    private void DisableAbility(Abilities ability, GameObject player)
+    {
+        if (ability.Equals(Abilities.Sprint))
+        {
+            SetSprintAbility(player, false);
+        }
+
+        else if (ability.Equals(Abilities.Jump))
+        {
+            SetJumpAbility(player, false);
+        }
+
+        else if (ability.Equals(Abilities.Dash))
+        {
+            SetDashAbility(player, false);
+        }
+    }
+
+    private void SetSprintAbility(GameObject player, bool enable)
+    {
+        var sprintController = player.GetComponent<CharacterSprint>();
+        var runController = player.GetComponent<CharacterRun>();
+        sprintController.AbilityPermitted = enable;
+        sprintController.RunStop();
+        runController.AbilityPermitted = enable;
+        runController.RunStop();
+    }
+
+    private void SetJumpAbility(GameObject player, bool enable)
+    {
+        player.GetComponent<CharacterJump>().AbilityPermitted = enable;
+    }
+
+    private void SetDashAbility(GameObject player, bool enable)
+    {
+        player.GetComponent<CharacterDash>().AbilityPermitted = enable;
     }
 }
