acfec54e516db424ae532113a285c810b2f09c03
xINVINCIBLEx <shubham170905@gmail.com>
2025-02-02 23:05:51 +0530

    Hell Scene

---
+++ Silence/Assets/Scripts/Sacrifice.cs
@@ -0,0 +1,53 @@
+using MoreMountains.CorgiEngine;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Sacrifice : MonoBehaviour
+{
+    [SerializeField] private float holdDuration = 2f;
+    [SerializeField] private KeyCode interactionKey = KeyCode.Space;
+    [SerializeField] private bool isActive = false;
+
+    private GameObject player;
+    private bool isSuccess = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        player = collision.gameObject;
+
+        isActive = true;
+    }
+
+    private void Update()
+    {
+        if (!isActive) return;
+
+        if (Input.GetKeyDown(interactionKey))
+        {
+            StartSacrifice();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isActive = false;
+    }
+
+    public async void StartSacrifice()
+    {
+        isSuccess = await QTEManager.Instance.HoldAsync(holdDuration, interactionKey);
+
+        if (isSuccess)
+        {
+            SacrificeCompleted();
+        }
+    }
+
+    private void SacrificeCompleted()
+    {
+        GameManager.Instance.LoseLife();
+        LevelManager.Instance.PlayerDead(player.GetComponent<Character>());
+    }
+}
