922deb322216bb22810aff054a1b9e28bc489b76
Yash <89540665+Y-a-s-h-b@users.noreply.github.com>
2025-02-02 16:33:07 +0530

    Audio Bar

--- Silence/Assets/Scripts/HealthBarUI.cs
+++ Silence/Assets/Scripts/HealthBarUI.cs
@@ -1,3 +1,4 @@
+using MoreMountains.CorgiEngine;
 using MoreMountains.Tools;
 using RengeGames.HealthBars;
 using UnityEngine;
@@ -5,18 +6,23 @@ using UnityEngine;
 public class HealthBarUI : MonoBehaviour
 {
     public RadialSegmentedHealthBar healthBar;
-    public MMProgressBar progressBar;
+    public LevelManager levelManager;
+    public AudioTesting audioTesting;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         healthBar = GetComponent<RadialSegmentedHealthBar>();
-        progressBar = GetComponentInParent<MMProgressBar>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(levelManager == null) levelManager = LevelManager.Instance;
+        else
+        {
+            audioTesting = levelManager.Players[0].GetComponent<AudioTesting>();
+        }
         //Debug.Log(progressBar.BarProgress);
-        healthBar.RemoveSegments.Value = healthBar.SegmentCount.Value - (progressBar.BarProgress * healthBar.SegmentCount.Value);
+        healthBar.RemoveSegments.Value = healthBar.SegmentCount.Value - ((audioTesting.audioBar * healthBar.SegmentCount.Value)/85);
     }
 }
