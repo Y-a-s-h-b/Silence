f9329b0bbcbafe1e818151310c53f3f94b43893f
Avashcodex <avash.biswas27@gmail.com>
2025-02-05 21:41:04 +0530

    bossEnemyUpdate

---
+++ Silence/Assets/Scripts/BossEnemyHealthModifier.cs
@@ -0,0 +1,33 @@
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using UnityEngine;
+
+public class BossEnemyHealthModifier : MonoBehaviour, MMEventListener<HealthDeathEvent>
+{
+    public Health health;
+
+    public GameObject[] bossEnemySpeakers;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = GetComponent<Health>();
+    }
+
+    private void OnEnable()
+    {
+        this.MMEventStartListening<HealthDeathEvent>();
+    }
+
+    private void OnDisable()
+    {
+        this.MMEventStopListening<HealthDeathEvent>();
+    }
+
+    public void OnMMEvent(HealthDeathEvent e)
+    {
+        if (e.AffectedHealth.transform.CompareTag("EnemyBreakableSpeaker"))
+        {
+            health.CurrentHealth -= 20f;
+        }        
+    }
+}
